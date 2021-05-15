12e7cf87140c74a3a231376dafe4912112f0d1e1
연훈 배 <sbaru1997@naver.com>
2021-05-15 16:43:23 +0900

    21.05.15 -2

--- nodeserver/myexpress/routes/index.js
+++ nodeserver/myexpress/routes/index.js
@@ -9,7 +9,7 @@ const storage = multer.diskStorage({
   filename: (req, file, callback) => {
     callback(null, file.originalname);
   }
-})
+});
 
 const uploader = multer({storage:storage});
 
@@ -17,9 +17,10 @@ router.get('/', (req, res, next) =>{
   res.render('index')
 });
 
-router.post('/upload_page', uploader.single('홍길동'), (req,res,next)=>{
-  console.log('파일 업로드');
+router.post('/upload_page', uploader.array('MarkerIMG'), (req,res,next)=>{
+  console.log(req.files),
+  console.log('파일 업로드'),
   res.redirect('/');
-})
+});
 
 module.exports = router;
